1353311d75dd9d48cd4b7a2debdd0f67ac4b8fa6
ZaidiArtem <74464917+ZaidiArtem@users.noreply.github.com>
2020-12-24 13:42:10 +0300

    first commit

---
+++ src/main/java/com/netcracker/Board/Events/BoardEventsApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.Board.Events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netcracker/Board/Events/BoardEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.Board.Events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
